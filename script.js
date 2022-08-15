7adce609ebc29f08c4b5666ea386d74a8a74c3ed
Pedro Victor <pedro20002victor@gmail.com>
2022-08-14 22:09:26 -0300

    Refatoring code | add API convert coin

--- script.js
+++ script.js
@@ -1,38 +1,67 @@
 const $ = document.querySelector.bind(document);
+const API_URL = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL"
 
 // Elements HTML
 let btnConverter = $('#btnConverter');
 let userInput = $("#userI");
 let resultado = $('.p_result');
+let updateCoinData = $("#updateCoin");
 
 const listCoins = $('#coins');
+
+// GET UPDATE COIN DATA
+async function UPDATE_DATA_COIN(coin) {
+   // Fetching coin data
+   const resp = await fetch(API_URL);
+   const resp_data = await resp.json();
+   // RETURN COIN DATA
+   const coinDataSelected = resp_data.USDBRL;
+   updateCoinData.innerHTML = `<p>Dados atualizados: ${coinDataSelected.create_date}</p>`;
+}
+
 
 const convertFunction = (entry, value) => {
    return parseFloat(entry * value).toFixed(2);
 }
 
-btnConverter.addEventListener('click', (e) => {
+
+
+btnConverter.addEventListener('click', async (e) => {
+   e.preventDefault();
+
+   // Fetching coin data
+   const resp = await fetch(API_URL);
+   const resp_data = await resp.json();
+
    e.preventDefault()
-   let valorEmReal;
    let valueSelected = listCoins.options[listCoins.selectedIndex].value;
 
-   if(userInput.value == 0) {
-      resultado.innerText = 'Preencha com algum valor valido'
-   }
-   else if(valueSelected === 'dolar') {
-      valorEmReal = convertFunction(userInput.value, 5.32)
-      resultado.innerText = `O valor convertido e ${valorEmReal} dolares`;
-   } 
-   else if (valueSelected === 'euro') {
-      valorEmReal = convertFunction(userInput.value, 5.35)
-      resultado.innerText = `O valor convertido e ${valorEmReal} euros`;
-   } 
-   else if (valueSelected === 'libra') {
-      valorEmReal = convertFunction(userInput.value, 6.32)
-      resultado.innerText = `O valor convertido e ${valorEmReal} libras`;
-   }
-   else if (valueSelected === 'bitcoin') {
-      valorEmReal = convertFunction(userInput.value, 109.230,00)
-      resultado.innerText = `O valor convertido e ${valorEmReal},00 bitcoins`
-   }
+
+
+   console.log(`Moeda selecionada ${valueSelected}`)
+
 });
+
+UPDATE_DATA_COIN();
+
+
+
+// if (userInput.value == 0) {
+//    resultado.innerText = 'Preencha com algum valor valido'
+// }
+// else if (valueSelected === 'dolar') {
+//    valorEmReal = convertFunction(userInput.value, 5.32)
+//    resultado.innerText = `O valor convertido e ${valorEmReal} dolares`;
+// }
+// else if (valueSelected === 'euro') {
+//    valorEmReal = convertFunction(userInput.value, 5.35)
+//    resultado.innerText = `O valor convertido e ${valorEmReal} euros`;
+// }
+// else if (valueSelected === 'libra') {
+//    valorEmReal = convertFunction(userInput.value, 6.32)
+//    resultado.innerText = `O valor convertido e ${valorEmReal} libras`;
+// }
+// else if (valueSelected === 'bitcoin') {
+//    valorEmReal = convertFunction(userInput.value, 109.230, 00)
+//    resultado.innerText = `O valor convertido e ${valorEmReal},00 bitcoins`
+// }
