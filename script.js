618f734e3010f277df2f3c5bfa119c65038cd010
Pedro Victor <pedro20002victor@gmail.com>
2022-08-14 22:51:19 -0300

    QUASE LA

--- script.js
+++ script.js
@@ -10,58 +10,43 @@ let updateCoinData = $("#updateCoin");
 const listCoins = $('#coins');
 
 // GET UPDATE COIN DATA
-async function UPDATE_DATA_COIN(coin) {
+async function UPDATE_DATA_COIN() {
    // Fetching coin data
    const resp = await fetch(API_URL);
    const resp_data = await resp.json();
+   console.log(resp_data);
    // RETURN COIN DATA
    const coinDataSelected = resp_data.USDBRL;
    updateCoinData.innerHTML = `<p>Dados atualizados: ${coinDataSelected.create_date}</p>`;
 }
 
-
+// ! BUG ABAIXO
 const convertFunction = (entry, value) => {
    return parseFloat(entry * value).toFixed(2);
 }
 
-
-
-btnConverter.addEventListener('click', async (e) => {
+btnConverter.addEventListener('click', async (e, coin) => {
    e.preventDefault();
 
    // Fetching coin data
    const resp = await fetch(API_URL);
    const resp_data = await resp.json();
+   const coinDataSelected = resp_data.USDBRL;
+
+   // OPTIONS AVAILABLE
+   const dolarValue = parseFloat(coinDataSelected.bid).toFixed(2);
+   const dolarNumber = Number(dolarValue);
 
    e.preventDefault()
    let valueSelected = listCoins.options[listCoins.selectedIndex].value;
+   console.log(`Moeda selecionada ${valueSelected}`);
 
-
-
-   console.log(`Moeda selecionada ${valueSelected}`)
+   if(valueSelected === 'dolar'){
+      console.log(`O dolar hoje esta com um valor de ${dolarValue}`)
+      
+      resultado.innerText = `A conversao e ${(userInput.value * dolarNumber).toFixed(2)}`
+   }
 
 });
 
 UPDATE_DATA_COIN();
-
-
-
-// if (userInput.value == 0) {
-//    resultado.innerText = 'Preencha com algum valor valido'
-// }
-// else if (valueSelected === 'dolar') {
-//    valorEmReal = convertFunction(userInput.value, 5.32)
-//    resultado.innerText = `O valor convertido e ${valorEmReal} dolares`;
-// }
-// else if (valueSelected === 'euro') {
-//    valorEmReal = convertFunction(userInput.value, 5.35)
-//    resultado.innerText = `O valor convertido e ${valorEmReal} euros`;
-// }
-// else if (valueSelected === 'libra') {
-//    valorEmReal = convertFunction(userInput.value, 6.32)
-//    resultado.innerText = `O valor convertido e ${valorEmReal} libras`;
-// }
-// else if (valueSelected === 'bitcoin') {
-//    valorEmReal = convertFunction(userInput.value, 109.230, 00)
-//    resultado.innerText = `O valor convertido e ${valorEmReal},00 bitcoins`
-// }
